c9df521f084343cda33db98ecef712fdaf39e5ea
IamNeoleon <kusherbaev.bakhtiyar@gmail.com>
2025-11-21 01:43:17 +0600

    some bugfix for workspaces

--- src/features/Workspace/CreateWorkspace.tsx
+++ src/features/Workspace/CreateWorkspace.tsx
@@ -1,7 +1,3 @@
-import { useModal } from "@/app/providers/ModalProvider"
-import { Button } from "@/components/ui/button"
-import { CreateDialog } from "./ui/CreateDialog"
-
 export const CreateWorkspace = () => {
 
 	return (
